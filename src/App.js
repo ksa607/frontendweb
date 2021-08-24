6d2cc24b64c790342f6894eaec56144a1ad6c680
Karine <karine.samyn@hogent.be>
2021-08-24 21:50:06 +0200

    Add transactions component + useMemo/callback

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
-import React, {useState, useMemo, useCallback} from "react";
-import Transaction from './components/Transaction';
+import React, {
+  useState,
+  useMemo,
+  useCallback
+} from "react";
+import Transactions from './components/Transactions';
 import AddTransactionForm from './components/AddTransactionForm';
 import Places from './components/Places';
-import {TRANSACTION_DATA, PLACE_DATA} from './mock-data';
+import {
+  TRANSACTION_DATA,
+  PLACE_DATA
+} from './mock-data';
 
 function App() {
   const [places, setPlaces] = useState(PLACE_DATA);
@@ -11,30 +18,34 @@ function App() {
   const [text, setText] = useState('');
   const [search, setSearch] = useState('');
 
-  const ratePlace =useCallback((id, rating) => {
-    const newPlaces=places.map(p=> p.id===id? {...p, rating}:p);
+  const ratePlace = useCallback((id, rating) => {
+    const newPlaces = places.map(p => p.id === id ? {
+      ...p,
+      rating
+    } : p);
     setPlaces(newPlaces)
   }, [places])
 
   const createTransaction = (user, place, amount, date) => {
     console.log("create transaction");
-    const newTransactions = [
-      {
+    const newTransactions = [{
         id: transactions.reduce((max, t) => (t.id > max ? t.id : max), 0) + 1,
-        user, place, amount, date:new Date(date)
+        user,
+        place,
+        amount,
+        date: new Date(date)
       },
       ...transactions,
     ]; // newest first
     setTransactions(newTransactions);
-    console.log("newtransactions", JSON.stringify(newTransactions));   
-    console.log("transactions", JSON.stringify(transactions));    
+    console.log("newtransactions", JSON.stringify(newTransactions));
+    console.log("transactions", JSON.stringify(transactions));
   };
 
-  const filteredTransactions = useMemo(()=>transactions.filter((t) =>{
-		console.log("filtering...");
-		return t.place.toLowerCase().includes(search.toLowerCase());
-	  })
-, [transactions, search])
+  const filteredTransactions = useMemo(() => transactions.filter((t) => {
+    console.log("filtering...");
+    return t.place.toLowerCase().includes(search.toLowerCase());
+  }), [transactions, search])
 
 
   /*
@@ -44,18 +55,17 @@ function App() {
     return t.place.toLowerCase().includes(search.toLowerCase())}), [transactions]})
     */
 
- console.log("render App")
-  return (
-    <div className="App">
-     <AddTransactionForm places={places} onSaveTransaction={createTransaction} />
-     <div className="m-5 flex">
-			<input type="text" value={text} onChange={(e) => setText(e.target.value)} className="flex-1" placeholder="search" />
-			<button type="button" onClick={()=>setSearch(text)}>Search</button>
-		</div>
-		{filteredTransactions.map((trans, index) => 
-        <Transaction {...trans} key={index}/>)}
-      <Places places={places} onRate={ratePlace}/>
-    </div>
+  console.log("render App")
+  return (   
+      <div className="App">
+        <AddTransactionForm places={places} onSaveTransaction={createTransaction} />
+        <div className="m-5 flex">
+         <input type="search" value={text} onChange={(e) => setText(e.target.value)} className="flex-1" placeholder="search" />
+          <button type="button" onClick={()=>setSearch(text)}>Search</button>
+        </div>
+        <Transactions transactions={filteredTransactions}/>
+        <Places places={places} onRate={ratePlace}/>
+      </div>
   );
 }
 
---
+++ src/components/Transactions.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { IoTrashOutline, IoPencil } from 'react-icons/io5';
+
+function Transaction({
+	id,
+	date,
+	amount,
+	user,
+	place}) {
+	
+	return (
+		<tr>
+			<td className="border w-1/4 px-4 py-2">{new Date(date).toLocaleDateString()}</td>
+			<td className="border w-1/4 px-4 py-2">{user}</td>
+			<td className="border w-1/4 px-4 py-2">{place}</td>
+			<td className="border w-1/4 px-4 py-2">{amount} &euro;</td>
+			<td className="border w-1/4 px-4 py-2">
+				<button>
+					<IoPencil  />
+				</button>
+			</td>
+			<td className="border w-1/4 px-4 py-2">
+				<button>
+					<IoTrashOutline />
+				</button>
+			</td>
+			{/* on {date} {user} {amount > 0 ? 'received' : 'spent'} {amount} {amount > 0 ? '' : 'at'} {place} */}
+		</tr>
+	);
+}
+
+const MemoizedTransaction =  React.memo(Transaction);
+
+export default function Transactions({
+    transactions
+}) {
+    return (
+		<table className="table-fixed m-auto">
+			<tbody>
+				{transactions.map((trans, i) => {
+					return (
+						<MemoizedTransaction
+							key={i}
+							{...trans}
+						/>
+					);
+				})}
+			</tbody>
+		</table>
+	);
+    }
