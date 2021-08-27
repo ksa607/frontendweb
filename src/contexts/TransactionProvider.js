e86a0fe4f0014708c2d6287cc2469a8c8a0d449d
Karine <karine.samyn@hogent.be>
2021-08-27 13:28:39 +0200

    provider

--- src/contexts/TransactionProvider.js
+++ src/contexts/TransactionProvider.js
@@ -2,21 +2,26 @@ import React, { createContext, useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import config from '../config.json';
 
-
 export const TransactionsContext = createContext();
 
 export const TransactionsProvider = (props) => {
 	const [currentTransaction, setCurrentTransaction] = useState({});
 	const [transactions, setTransactions] = useState([]);
+	const [error, setError] = useState();
+	const [loading, setLoading] = useState(false); //error state of throw error
 
 	const refreshTransactions = useCallback(async () => {
 		try {
+			setError();
+			setLoading(true);
 			const { data } = await axios.get(
 				`${config.base_url}transactions?limit=25&offset=0`
 			);
 			setTransactions(data.data);
+			setLoading(false);
 			return data.data;
 		} catch (error) {
+			setLoading(false);
 			throw error;
 		}
 	}, []);
